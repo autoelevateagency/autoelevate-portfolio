e65cbbd870891078473ce75fe270e0acd2bfaf40
Muhammad Taha <muhammad9675581@gmail.com>
2026-09-15 02:50:54 +0500

    domain issue

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: false,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.goautoelevate.com" }],
-        destination: "https://goautoelevate.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
